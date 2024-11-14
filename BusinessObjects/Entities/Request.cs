b5f0ea59f01ad85466702178044f782be5026145
NhatMinh <mcpegunny@gmail.com>
2024-11-14 08:08:36 +0700

    fix request

--- BusinessObjects/Entities/Request.cs
+++ BusinessObjects/Entities/Request.cs
@@ -11,6 +11,8 @@ public partial class Request
 
     public string? Status { get; set; }
 
+    public bool? Export { get; set; }
+
     public virtual Account? Account { get; set; }
 
     public virtual ICollection<RequestDetail> RequestDetails { get; set; } = new List<RequestDetail>();
